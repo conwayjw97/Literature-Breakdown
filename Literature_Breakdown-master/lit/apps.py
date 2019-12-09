1a5780399a5fce467bf3bb8d81e5b511da39191d
James Conway (student) <2247492C@student.gla.ac.uk>
2019-12-09 15:05:41 +0000

    Small cleanup

--- Literature_Breakdown-master/lit/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LitConfig(AppConfig):
-    name = 'lit'
--- Literature_Breakdown-master/lit/templatetags/lit_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from lit.models import Category
-
-register = template.Library()
-
-@register.inclusion_tag('lit/cats.html')
-def get_category_list(cat=None):
-    return {'cats': Category.objects.all(),
-            'act_cat': cat}
